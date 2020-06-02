8da46d62b250a46803356c047eeeb5c0bc309c00
Bhargav-Kumar-Murala <bhargav.murala@apntbs.com>
2020-06-02 11:20:49 +0530

    commit1

---
+++ lib/result.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 15) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 20) {
+      resultText = 'You are... strange?!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Try Again?',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
